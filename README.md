59685190d4498d55c7bf70e83158c0f5c4f2cbad
Tony Chan <tony.chan@vonage.com>
2020-12-15 17:41:43 -0800

    initial

---
+++ README.md
@@ -0,0 +1,34 @@
+# Google Sentiment Analysis for text from Vonage API Client SDK in-app messaging, Vonage Messaging API, Vonage SMS API
+
+This sample Node.JS code shows how to use Google Sentiment Analysis engine for text from Vonage API Client SDK in-app messaging, Vonage Messaging API, and Vonage SMS API . Multiple languages are supported.
+
+## Set up
+
+### Google credentials
+
+Retrieve your Google credentials for your application
+
+_TBD ...._
+
+Save the credentials .json file on your local application folder.
+
+Create a .env file with the entry specifying the .json file name</br>
+**_GOOGLE_APPLICATION_CREDENTIALS=./xxxxx.json_**
+
+### Node packages
+
+Add necessary packages:
+```bash
+npm install
+```
+
+## Test sample transcriptions
+
+Enter your text and corresponding language code in attached sample sentiment-analysis.js code.
+
+Run the application:
+```bash
+node sentiment-analysis
+```
+
+
---
+++ sentiment-analysis.js
@@ -0,0 +1,68 @@
+'use strict'
+require('dotenv').load()
+
+//------------
+
+const nlp = require('@google-cloud/language');
+
+//--- Function declaration for text to be sentiment analyzed
+
+async function analyzeSentimentOfText(text, language) {
+
+  const client = new nlp.LanguageServiceClient();
+
+  const document = {
+    content: text,
+    language: language,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the document
+  const [result] = await client.analyzeSentiment({document});
+
+  return ([result]);
+
+  // const sentiment = result.documentSentiment;
+  // console.log('Document sentiment:');
+  // console.log(`  Score: ${sentiment.score}`);
+  // console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+  // const sentences = result.sentences;
+  // sentences.forEach(sentence => {
+  //   console.log(`Sentence: ${sentence.text.content}`);
+  //   console.log(`  Score: ${sentence.sentiment.score}`);
+  //   console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  // });
+
+}
+
+//============= Sample sentiment analysis function call ================
+
+// text to be sentiment analyzed
+const text = 'Hello, you are an awesome agent! You provide an excellent service! Your company is fantastic!';
+
+// language code as listed here: https://cloud.google.com/natural-language/docs/languages#sentiment_analysis
+const language = 'en';
+
+
+// -- function call
+(async () => {
+	const [analysis] = await analyzeSentimentOfText(text, language);
+
+  // This section results may be enough for your application?
+  const sentiment = analysis.documentSentiment;
+  console.log('Document sentiment:');
+  console.log(`  Score: ${sentiment.score}`);
+  console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+  // This section may not be needed for your application?
+  const sentences = analysis.sentences;
+  sentences.forEach(sentence => {
+    console.log(`Sentence: ${sentence.text.content}`);
+    console.log(`  Score: ${sentence.sentiment.score}`);
+    console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  });
+
+})();
+
+//--------
